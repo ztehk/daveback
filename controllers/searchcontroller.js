582c6e8306fce814a79abf09585005a83a9ebc6b
unknown <shaltyshiku@gmail.com>
2023-09-11 23:51:02 +0100

    changes

--- controllers/searchcontroller.js
+++ controllers/searchcontroller.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const Music = require('../models/musicmodel');
 const Lyric = require('../models/LyricsModel');
 const Album = require('../models/albummodel');
+const News = require('../models/newsmodel');
 
 const searchfun = async (req, res) => {
 	const { query } = req.query;
@@ -23,11 +24,13 @@ async function search(query) {
 	const musicResults = await Music.find({ title: searchRegex }).exec();
 	const lyricsResults = await Lyric.find({ title: searchRegex }).exec();
 	const GospelResults = await Album.find({ title: searchRegex }).exec();
+	const NewsResults = await News.find({ title: searchRegex }).exec();
 
 	const combinedResults = [
 		...musicResults,
 		...GospelResults,
 		...lyricsResults,
+		...NewsResults,
 	];
 
 	return combinedResults;
