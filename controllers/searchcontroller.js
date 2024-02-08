b1a377bea8beecc7b2f42e32a40f5942d87c6eb5
ztech <134311548+Daniel-tech-cmd@users.noreply.github.com>
2024-02-08 10:56:36 +0100

    Update

--- controllers/searchcontroller.js
+++ controllers/searchcontroller.js
@@ -17,30 +17,43 @@ const searchfun = async (req, res) => {
 };
 
 async function search(query) {
-	const regexQuery = query
-		.split(' ')
-		.map(word => `.*${word}.*`)
-		.join('|');
-	const searchRegex = new RegExp(regexQuery, 'i');
+    const regexQuery = query
+        .split(' ')
+        .map(word => `.*${word}.*`)
+        .join('|');
+    const searchRegex = new RegExp(regexQuery, 'i');
 
-	const musicResults = await Music.find({ title: searchRegex }).exec();
-	const lyricsResults = await Lyric.find({ title: searchRegex }).exec();
-	const GospelResults = await Album.find({ title: searchRegex }).exec();
-	const NewsResults = await News.find({ title: searchRegex }).exec();
-	const SportResults = await Sport.find({ title: searchRegex }).exec();
-	const Album2Results = await Album2.find({ title: searchRegex }).exec();
+    // Search in Music collection based on title or artist
+    const musicResults = await Music.find({ $or: [{ songOwner: searchRegex }, { title: searchRegex }] }).exec();
 
-	const combinedResults = [
-		...SportResults,
-		...GospelResults,
-		...lyricsResults,
-		...lyricsResults,
-		...Album2Results,
-		...musicResults,
-	];
- 	
-	return combinedResults.reverse();;
+    // Search in Lyric collection based on title or artist
+    const lyricsResults = await Lyric.find({ $or: [{ songOwner: searchRegex }, { title: searchRegex }] }).exec();
+
+    // Search in Album collection based on title or artist
+    const gospelResults = await Album.find({ $or: [{ songOwner: searchRegex }, { title: searchRegex }] }).exec();
+
+    // Search in News collection based on title
+    const newsResults = await News.find({ title: searchRegex }).exec();
+
+    // Search in Sport collection based on title
+    const sportResults = await Sport.find({ title: searchRegex }).exec();
+
+    // Search in Album2 collection based on title
+    const album2Results = await Album2.find({ title: searchRegex }).exec();
+
+    // Combine the results from all collections
+    const combinedResults = [
+        ...sportResults,
+        ...gospelResults,
+        ...lyricsResults,
+        ...newsResults,
+        ...album2Results,
+        ...musicResults,
+    ];
+
+    return combinedResults.reverse();
 }
+
 module.exports = {
 	searchfun,
 };
