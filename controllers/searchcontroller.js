3fc10ed409061a278692839de0e2ac786fa53223
ztech <134311548+Daniel-tech-cmd@users.noreply.github.com>
2024-02-09 10:16:56 +0100

    Update searchcontroller.js

--- controllers/searchcontroller.js
+++ controllers/searchcontroller.js
@@ -20,29 +20,42 @@ async function search(query) {
     // Replace "ft" with an empty string in the query
     const sanitizedQuery = query.replace(/\bft\b/gi, '');
 
-    const regexQuery = sanitizedQuery
-        .split(' ')
-        .map(word => `.*${word}.*`)
-        .join('|');
-    const searchRegex = new RegExp(regexQuery, 'i');
+    const searchWords = sanitizedQuery.split(' ');
+
+    const searchRegexes = searchWords.map(word => new RegExp(`.*${word}.*`, 'i'));
 
     // Search in Music collection based on title or songOwner (with "ft" filtered out)
-    const musicResults = await Music.find({ $or: [{ title: searchRegex }, { songOwner: searchRegex }] }).exec();
+    const musicResults = await Music.find({
+        $or: [
+            { title: { $all: searchRegexes } },
+            { songOwner: { $all: searchRegexes } }
+        ]
+    }).exec();
 
     // Search in Lyric collection based on title or songOwner (with "ft" filtered out)
-    const lyricsResults = await Lyric.find({ $or: [{ title: searchRegex }, { songOwner: searchRegex }] }).exec();
+    const lyricsResults = await Lyric.find({
+        $or: [
+            { title: { $all: searchRegexes } },
+            { songOwner: { $all: searchRegexes } }
+        ]
+    }).exec();
 
     // Search in Album collection based on title or songOwner (with "ft" filtered out)
-    const gospelResults = await Album.find({ $or: [{ title: searchRegex }, { songOwner: searchRegex }] }).exec();
+    const gospelResults = await Album.find({
+        $or: [
+            { title: { $all: searchRegexes } },
+            { songOwner: { $all: searchRegexes } }
+        ]
+    }).exec();
 
     // Search in News collection based on title
-    const newsResults = await News.find({ title: searchRegex }).exec();
+    const newsResults = await News.find({ title: { $all: searchRegexes } }).exec();
 
     // Search in Sport collection based on title
-    const sportResults = await Sport.find({ title: searchRegex }).exec();
+    const sportResults = await Sport.find({ title: { $all: searchRegexes } }).exec();
 
     // Search in Album2 collection based on title
-    const album2Results = await Album2.find({ title: searchRegex }).exec();
+    const album2Results = await Album2.find({ title: { $all: searchRegexes } }).exec();
 
     // Combine the results from all collections
     const combinedResults = [
@@ -56,6 +69,7 @@ async function search(query) {
 
     return combinedResults.reverse();
 }
+
 
 
 module.exports = {
